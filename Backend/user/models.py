728bc14520d465e7d35ec7cdbb3417b335b792f5
Kishor404 <kishor404@outlook.in>
2026-02-08 15:48:33 +0530

    bug fix

--- Backend/user/models.py
+++ Backend/user/models.py
@@ -47,9 +47,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     customer_code = models.CharField(
         max_length=20,
-        unique=True,
-        null=True,
-        blank=True
+        unique=True
     )
 
     name = models.CharField(max_length=150)
